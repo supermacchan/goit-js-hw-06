975ef3bafc9f34ac0286d1589d726f0f1816a9c6
Olga Birkovska <birkovskaya92@ukr.net>
2022-09-10 16:59:43 +0300

    Update task-08.js
    done

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,24 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+    event.preventDefault();
+
+    const formElements = event.currentTarget.elements;
+    const emailName = formElements.email.name;
+    const emailValue = formElements.email.value;
+    const passwordName = formElements.password.name;
+    const passwordValue = formElements.password.value;
+
+    if (emailValue !== '' && passwordValue !== '') {
+        console.log(`
+        ${emailName}: ${emailValue}
+        ${passwordName}: ${passwordValue}
+        `);
+
+        event.currentTarget.reset();
+    } else {
+        window.alert('All fields must be filled!');
+    }
+}
