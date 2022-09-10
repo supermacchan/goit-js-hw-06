88fbed424db2ad8600725d78de11e219827fc1b9
Olga Birkovska <birkovskaya92@ukr.net>
2022-09-10 16:16:57 +0300

    Update task-06.js
    done

--- src/js/task-06.js
+++ src/js/task-06.js
@@ -3,12 +3,12 @@ const inputEl = document.querySelector('#validation-input');
 inputEl.addEventListener('blur', onBlur);
 
 function onBlur(event) {
-    if (event.currentTarget.value.length === event.currentTarget.dataset.length) {
-        event.currentTarget.classList.add('.valid');
-    } else {
-        event.currentTarget.classList.add('.invalid');
-    }
-    console.log(event.currentTarget.value.length);
-    console.log(inputEl.dataset.length === inputEl.value.length);
-}
+    const requiredLength = Number(event.currentTarget.dataset.length);
+    if (event.currentTarget.value.length === requiredLength) {
+        event.currentTarget.classList.remove('invalid');
+        event.currentTarget.classList.add('valid');
 
+    } else {
+        event.currentTarget.classList.add('invalid');
+    }
+}
