38bfd49875fac5010aca2c2aad51a2e485e914b7
Olga Birkovska <birkovskaya92@ukr.net>
2022-09-10 22:05:08 +0300

    Update task-09.js

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const colorChangingBtn = document.querySelector('.change-color');
+const colorRef = document.querySelector('.color');
+
+colorChangingBtn.addEventListener('click', onBtnClick);
+
+function onBtnClick(event) {
+  const newColor = getRandomHexColor();
+  document.body.style.backgroundColor = newColor;
+  colorRef.textContent = newColor;
+}
